8711dd42a3b89f05fa6ba3e04a87bbaeb75081a3
tangzhongaogit <zhongaotang@gmail.com>
2015-11-06 18:31:42 -0500

    second commit

--- ApacheCMDA-Frontend/target/scala-2.10/src_managed/main/views/html/climate/header.template.scala
+++ ApacheCMDA-Frontend/target/scala-2.10/src_managed/main/views/html/climate/header.template.scala
@@ -71,14 +71,14 @@ Seq[Any](format.raw/*18.4*/("""
 			</div>
 			<div class="navbar-collapse collapse" style="height:">
 				<form class="navbar-form navbar-right" role="register">
-					<a class="btn btn-primary" href="climate/sign" role="button">Register &raquo;</a>
+					<a class="btn btn-primary" href="/climate/sign" role="button">Register &raquo;</a>
 				</form>
 				<ul class="nav navbar-nav navbar-right">
 					<form class="navbar-form navbar-right" role="login">
 							<div class="form-group">
 							<input class="form-control" id="ex1" type="text"  placeholder="Username">
 							<input type="password" class="form-control" placeholder="Password">
-						</div>
+							</div>
 						<button type="submit" class="btn btn-success">Login</button>
 					</form>
 					<li class="dropdown"><a href="#"
@@ -131,10 +131,10 @@ Seq[Any](format.raw/*18.4*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Fri Nov 06 17:33:20 EST 2015
+                    DATE: Fri Nov 06 18:28:11 EST 2015
                     SOURCE: C:/Users/Joe/Desktop/activator and projects/ApacheCMDA-Frontend/app/views/climate/header.scala.html
-                    HASH: 01bc64bc31241020bc635f61e46f401c27c93ed0
-                    MATRIX: 3201->1222|3297->1224|3840->1731|3855->1737|3906->1766|4798->2622|4813->2628|4878->2671|5160->2917|5175->2923|5229->2955|5310->3000|5325->3006|5381->3040|5466->3133|5502->3201|5539->3236|5575->3274|5613->3376|5651->3483|5687->3500|5722->3516|5953->3711|5968->3717|6022->3749|6095->3786|6110->3792|6160->3820|6237->3861|6252->3867|6307->3900
+                    HASH: 37aa4665aca28dc873f2584f668958856376eb16
+                    MATRIX: 3201->1222|3297->1224|3840->1731|3855->1737|3906->1766|4800->2624|4815->2630|4880->2673|5162->2919|5177->2925|5231->2957|5312->3002|5327->3008|5383->3042|5468->3135|5504->3203|5541->3238|5577->3276|5615->3378|5653->3485|5689->3502|5724->3518|5955->3713|5970->3719|6024->3751|6097->3788|6112->3794|6162->3822|6239->3863|6254->3869|6309->3902
                     LINES: 56->18|59->18|70->29|70->29|70->29|88->47|88->47|88->47|96->55|96->55|96->55|97->56|97->56|97->56|100->59|101->60|102->61|103->62|104->63|105->64|106->65|107->66|113->72|113->72|113->72|114->73|114->73|114->73|115->74|115->74|115->74
                     -- GENERATED --
                 */
